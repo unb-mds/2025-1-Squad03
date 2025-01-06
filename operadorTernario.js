1130d5edef8162cd0b90fb42836f4012192c7402
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-01-06 15:04:42 -0300

    operador ternario

---
+++ operadorTernario.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let numero = 10
+let resultado = (numero%2 ? "par" : "impar")
+console.log(resultado)
+
+numero = 11
+resultado = (numero%2 ? "par" : "impar")
+console.log(resultado)
--- operadoresLogicos.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-
-// && -> and
-// || -> ou
-// ! -> not
