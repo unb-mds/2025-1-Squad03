76ab55294c8900a23b05904620047f79dece0e25
wallace barros da  cruz <w.wallacebarros.cruz@example.com>
2025-01-06 10:27:11 -0300

    var, let, const

--- strictMode.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-
-var nome = "Wallace"
-console.log("Nome: " + nome)
---
+++ tiposDeVariaveis.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Variavel espaço reservado dentro da memoria principal que armazena um valor x.
+
+var nome = "Wallace" // var armazena um dado que pode ser alterado, pode ser acessada fora do seu escopo
+
+let escopo = "restrito" // let armazena um dado que pode ser alterado, não pode ser acessadofora do seu escopo
+
+const constante = "nao se altera" // const atribui um valor que nao pode ser alterado depois
+
+console.log("var: "+nome+", let: "+escopo+", const: "+constante)
