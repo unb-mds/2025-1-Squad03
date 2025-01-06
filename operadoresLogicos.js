b6ff4644f540986e292d4004da30bb7171d3ad82
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-01-06 14:55:13 -0300

    operadores logicos

---
+++ operadoresLogicos.js
@@ -0,0 +1,5 @@
+"use strict"
+
+// && -> and
+// || -> ou
+// ! -> not
--- operaforesRelacionais.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-console.log("1 maior que 2: "+(1>2))
-console.log("1 menor que 2: "+(1<2))
-console.log("1 maior ou igual a 2: "+(1>=2))
-console.log("1 menor ou igual a 2: "+(1<=2))
-console.log("1 e igual a '1'"+(1=='1'))
-console.log("1 e diferente de '1'"+(1!='1'))
-console.log("1 e igual e do mesmo tipo de '1'"+(1==='1'))
