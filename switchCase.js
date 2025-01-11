1620b16605c89ba1e5490fbe5dff100ccfe2c4d0
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-01-11 10:17:09 -0300

    switch case

---
+++ switchCase.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const flag = 1
+
+switch(flag) {
+    case 1:
+        console.log("flag = 1")
+        break
+    case 2:
+        console.log("flag = 2")
+        break
+    case 3:
+        console.log("flag = 3")
+        break
+    default:
+        console.log("flag diferente de 1,2,3")
+        break
+}
