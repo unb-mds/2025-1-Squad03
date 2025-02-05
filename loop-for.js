c3523bb70ae5013ac7772102f8de6b37dae46c11
WallaceBarrosDev <wallacebarro.cruz@gmail.com>
2025-02-05 23:54:30 +0000

    apredendo: for

---
+++ loop-for.js
@@ -0,0 +1,15 @@
+// estruutura de repetição // 
+
+// cada repetição que o loop execultar e uma itereção.
+// pode ser definido ou indefinido / sei a quantifdade ou não sei a quantidade.
+
+// loop definido for.
+// loop indefinido while ou do while.
+
+// FOR(inicializaçã;condição;controle) {
+// codigo a ser iterado ...
+// }
+
+for (let index = 0; index <= 10; index++) {
+    console.log(index)
+}
