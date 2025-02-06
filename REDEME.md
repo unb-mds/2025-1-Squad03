17767d43d747ad998c0341debb5df46e4fff20d5
wallace <w.wallacebarros.cruz@gmail.com>
2025-02-06 11:12:00 -0300

    fix

--- REDEME.md
+++ REDEME.md
@@ -1,1 +1,8 @@
-# meu redeme
+# Estrutura de repetição
+
+---
+
+- cada repetição que o loop execultar e uma **itereção**.
+- pode ser **definido** ou **indefinido** / sei a quantidade ou não sei a quantidade.
+- loop definido **for, forin ou forof**.
+- loop indefinido while ou do while.
--- loop-for.js
+++ loop-for.js
@@ -1,12 +1,4 @@
-// estruutura de repetição // 
-
-// cada repetição que o loop execultar e uma itereção.
-// pode ser definido ou indefinido / sei a quantifdade ou não sei a quantidade.
-
-// loop definido for.
-// loop indefinido while ou do while.
-
-// FOR(inicializaçã;condição;controle) {
+ // FOR(inicializaçã;condição;controle) {
 // codigo a ser iterado ...
 // }
 
