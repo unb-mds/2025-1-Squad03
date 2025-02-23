b2ac6cf4d774d958754e72defd92b99a23a10870
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-02-23 15:14:50 +0000

    feat: while e do while

---
+++ wile-dowile.js
@@ -0,0 +1,13 @@
+let flag = 1;
+
+while (flag <= 10) {
+    console.log(`${flag}° iteração`);
+    flag ++;
+};
+
+flag = 1;
+
+do {
+    console.log(`${flag}° interação`);
+    flag ++;
+} while (flag <= 10);
